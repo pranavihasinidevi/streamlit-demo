9515702443bee1d06928d661486a30d4674b8ff9
pranavihasinidevi <pranavihasinidevi@gmail.com>
2026-06-12 11:46:01 +0530

    Update aoo.py

--- aoo.py
+++ aoo.py
@@ -1,7 +1,8 @@
 import streamlit as st
-st.title("Welcome to Streamlit")
-name = st.text_input("Enter your name")
-if name: st.success(f"Hello {name}")
-age = st.slider("Select your age", 1, 100)
-st.write("Age:", age)
-if st.button("Celebrate"): st.balloons()s
+
+st.title("Hello World")
+
+st.write("Welcome to my first Streamlit app!")
+
+if st.button("Celebrate"):
+    st.balloons()
